1f85ea9d1160534f1768567d2c0ffcf98c2a582c
OffsetDEV <50267223+OffsetDEV@users.noreply.github.com>
2019-05-25 11:48:20 +0200

    Code-5-master
    First push.

---
+++ Code5-master/README.md
@@ -0,0 +1,2 @@
+# Code5 Doors
+Use /code5 to open your front left door while close to your cruiser.
---
+++ Code5-master/__resource.lua
@@ -0,0 +1,3 @@
+resource_manifest_version "679-44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script "client.lua"
